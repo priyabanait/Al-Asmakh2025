34a2d6dee6c29feb76cf9fbc299917f681769e20
root <moinmisba92@gmail.com>
2026-03-14 19:05:34 +0000

    new code

--- src/app/services/facilities-management/page.js
+++ src/app/services/facilities-management/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import Header from '../../../components/Header'
-import Services from '../../../components/Services'
+import Services from '../../../components/Services-lease'
 import Footer from '../../../components/Footer'
 
 export default function FacilitiesManagementPage() {
